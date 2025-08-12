71e77d6c72df5abf721ae8336dfbef20d74d60f0
Mohit <mohitgudadhe2803@gmail.com>
2025-08-12 21:49:30 +0530

    new file

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,29 @@
+import Navbar from "~/componets/Navbar";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
-export function meta({}: Route.MetaArgs) {
+
+export function meta({ }: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resumind" },
+    { name: "description", content: "Smart feedback from your dream job" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return<main className="bg-[url('/image/bg-main.svg')] bg-cover">
+    <Navbar />
+    <section className="main-section">
+      <div className="page-heading">
+        <h1>Track Your Application & Resume Rating</h1>
+        <h2>Remove your submission and checked AI powered feedback.</h2>
+      </div>
+      {/* RESUME CARD */}
+      <div>
+        <div>
+          
+        </div>
+      </div>
+
+    </section>
+  </main>
 }
